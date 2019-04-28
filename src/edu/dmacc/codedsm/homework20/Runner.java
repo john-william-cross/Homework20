f36972e252ed934a15a947e275aafa9492ff11a5
John Cross <jwcross1@dmacc.edu>
2019-04-28 10:38:33 -0500

    used reduce instead of flatmap + collect to count chars in strings

--- src/edu/dmacc/codedsm/homework20/Runner.java
+++ src/edu/dmacc/codedsm/homework20/Runner.java
@@ -12,23 +12,30 @@ public class Runner {
         List<Integer> lengthOfIndividualStrings = animals.stream()
                 .map(x -> x.length())
                 .collect(toList());
-        System.out.println(lengthOfIndividualStrings);
+        System.out.println(lengthOfIndividualStrings);  //outputs [3, 3, 4, 6, 6]
+
 
         List<Character> result = animals.stream()
                 .flatMap(word -> word.chars().mapToObj(i -> (char) i))
                 .collect(toList());
-        System.out.println(result);
+        System.out.println(result); //outputs [D, o, g, C, a, t, B, i, r, d, M, o, n, k, e, y, D, o, n, k, e, y]
 
         List<String> startsWithD = animals.stream()
                 .filter(x -> x.startsWith("D"))
                 .collect(toList());
-        System.out.println(startsWithD);
+        System.out.println(startsWithD); //outputs [Dog, Donkey]
 
-        int count = animals.stream()
+        int count1 = animals.stream()
+                .map(String::length)
+                .reduce(0,(m1,m2)->m1+m2);
+        System.out.println(count1); //outputs 22 using reduce rather than the first way I used below
+
+        /*int count = animals.stream()
                 .flatMap(word -> word.chars().mapToObj(i -> (char) i))
                 .collect(toList())
                 .size();
-        System.out.println(count);
+        System.out.println(count);   outputs 22*/
+
 
     }
 }
