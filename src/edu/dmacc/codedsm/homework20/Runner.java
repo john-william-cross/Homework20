777fe6edfd803d605e92563a449299eaf3b36f03
John Cross <jwcross1@dmacc.edu>
2019-04-28 07:40:55 -0500

    formatting changes

--- src/edu/dmacc/codedsm/homework20/Runner.java
+++ src/edu/dmacc/codedsm/homework20/Runner.java
@@ -1,23 +1,15 @@
 package edu.dmacc.codedsm.homework20;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.toList;
 
 
 public class Runner {
     public static void main(String[] args) {
 
-
         List<String> animals = Arrays.asList("Dog", "Cat", "Bird", "Monkey", "Donkey");
-
-        animals.stream();
 
         List<Integer> lengthOfIndividualStrings = animals.stream()
                 .map(x -> x.length())
@@ -25,7 +17,7 @@ public class Runner {
         System.out.println(lengthOfIndividualStrings);
 
         List<Character> result = animals.stream()
-                .flatMap(word -> word.chars().mapToObj(i -> (char)i))
+                .flatMap(word -> word.chars().mapToObj(i -> (char) i))
                 .collect(toList());
         System.out.println(result);
 
@@ -35,12 +27,10 @@ public class Runner {
         System.out.println(startsWithD);
 
         int count = animals.stream()
-                .flatMap(word -> word.chars().mapToObj(i -> (char)i))
+                .flatMap(word -> word.chars().mapToObj(i -> (char) i))
                 .collect(toList())
                 .size();
         System.out.println(count);
 
-
     }
-
 }
